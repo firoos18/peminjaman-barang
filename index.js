b84478740a5531af89445246a24b2a69f239319d
firoos18 <itsfiroos@gmail.com>
2024-07-05 08:23:18 +0700

    bug : fixing CORS

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", async (req, res, next) => {
   res.send("Hello, from backend");
 });
